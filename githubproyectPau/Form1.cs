ceaf128804fac5305a5b276ec0233b750627f438
paulopcri <paulopcri@monlau.com>
2020-02-12 15:56:34 +0100

    version 0.0.1

---
+++ githubproyectPau/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace githubproyectPau
+{
+    //pau lopez
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int dni = Convert.ToInt32(textBox1.ToString());
+            char letter = funtionDNI(dni);
+            textBox2.Text = letter.ToString();
+
+        }
+        private char funtionDNI(int dni)
+        {
+            char letter = 'A';
+            return letter;
+
+        }
+    }
+}
